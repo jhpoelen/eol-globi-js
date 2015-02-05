9aca9a1b86ff1c96632e623af79f8fc0b0e4ab39
G. Bodenschatz <coding@46halbe.de>
2015-02-05 10:26:14 +0100

    Initial widget state

---
+++ widget.js
@@ -0,0 +1,27 @@
+(function($, window, document, undefined) {
+    'use strict';
+
+    var pluginName = 'interactions',
+        defaults = {};
+
+    function Plugin(element, options) {
+        this.element = element;
+
+        this.settings = $.extend({}, defaults, options);
+        this._defaults = defaults;
+        this._name = pluginName;
+        this.init();
+    }
+
+    $.extend(Plugin.prototype, {
+        init: function() {}
+    });
+
+    $.fn[pluginName] = function(options) {
+        return this.each(function() {
+            if (!$.data(this, 'plugin_' + pluginName)) {
+                $.data(this, 'plugin_' + pluginName, new Plugin(this, options));
+            }
+        });
+    };
+})(jQuery, window, document);
