6db5b2e806c69f6ba09d5545543a51f1bf19c6a0
G. Bodenschatz <coding@46halbe.de>
2015-03-12 22:02:47 +0100

    [BUGFIX] Fix wrong info urls in result list

--- globi.js
+++ globi.js
@@ -898,14 +898,14 @@ globi.extend(globi.PaginatedDataFetcher.prototype, {
                 var commonName = data['commonName'] ? data['commonName'] : '',
                     scientificName = data['scientificName'] ? data['scientificName'] : '',
                     thumbnailURL = data['thumbnailURL'] ? data['thumbnailURL'] : '',
-                    infoUrl = data['thumbnailUrl'] ? data['thumbnailUrl'] : '',
+                    infoURL = data['infoURL'] ? data['infoURL'] : '',
                     imageURL = data['imageURL'] ? data['imageURL'] : '';
 
                 return [
                     '<div class="scientific-name">' + scientificName + '</div>',
                     '<div class="taxon-image"><a target="_blank" href="' + imageURL + '"><img height="50px" src="' + thumbnailURL + '" /></a></div>',
                     '<div class="common-name">' + commonName + '</div>',
-                    '<div class="more-info"><a href="' + infoUrl + '">more info</a></div>'
+                    '<div class="more-info"><a target="_blank" href="' + infoURL + '">more info</a></div>'
                 ];
             }
         },
